5b3bfaa9b3cb79a1a0b949c9022eb807cacedc8b
meshackmesto <meshackorina93@gmail.com>
2024-05-27 14:45:56 +0300

    done

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
-
+    """Returns a list of even numbers from a list of integers."""
+    return [num for num in num_list if num % 2 == 0]
 def make_exclamation(sentence_list):
-    pass
+    return [sent + "!" for sent in sentence_list]
